95b3d468211201cbd484876ea7635da54ac163b6
GuRuGu <peter.k.nets@gmail.com>
2021-08-01 12:58:15 +0300

    Load data and show it

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <div class="App">Hello</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.hnpwa.com/v0/news/1.json")
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(data);
+
+  return (
+    <div className="App">
+      <div>Time Added</div>
+      <div>Title</div>
+      <div>Domain</div>
+      {data.map(item => (
+        <React.Fragment key={item.id}>
+          <div>{item.time}</div>
+          <div>{item.title}</div>
+          <div>{item.domain}</div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 };
 
 export default App;
